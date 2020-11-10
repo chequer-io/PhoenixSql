6686008e661c0f36f1e40ae17dac0af65e01f90b
Evan <evan@chequer.io>
2020-11-10 20:06:37 +0900

    Deparser w.i.p

--- PhoenixSql/Internal/ScriptWriter.cs
+++ PhoenixSql/Internal/ScriptWriter.cs
@@ -1,7 +1,64 @@
-﻿namespace PhoenixSql.Internal
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PhoenixSql.Internal
 {
-    public class ScriptWriter
+    internal sealed class ScriptWriter
     {
-        
+        private readonly StringBuilder _builder;
+
+        public ScriptWriter()
+        {
+            _builder = new StringBuilder();
+        }
+
+        public ScriptWriter WriteSpace()
+        {
+            if (_builder.Length > 0 && _builder[^1] != ' ')
+            {
+                _builder.Append(' ');
+            }
+
+            return this;
+        }
+
+        public ScriptWriter Write(object value)
+        {
+            _builder.Append(value);
+            return this;
+        }
+
+        public ScriptWriter Write(char value)
+        {
+            _builder.Append(value);
+            return this;
+        }
+
+        public ScriptWriter Write(string value)
+        {
+            _builder.Append(value);
+            return this;
+        }
+
+        public void WriteJoin<T>(string delimiter, IEnumerable<T> elements, System.Action<ScriptWriter, T> action)
+        {
+            using IEnumerator<T> enumerator = elements.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+                return;
+
+            action(this, enumerator.Current);
+
+            while (enumerator.MoveNext())
+            {
+                Write(delimiter);
+                action(this, enumerator.Current);
+            }
+        }
+
+        public override string ToString()
+        {
+            return _builder.ToString();
+        }
     }
 }
